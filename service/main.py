d66749639bde052934cef03dbde881cce1c3f40c
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2013-12-09 19:50:11 +1100

    ADD: App now serves (other devices on network can connect)

--- service/main.py
+++ service/main.py
@@ -1,5 +1,6 @@
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djandro.settings")
 from django.core.management.commands.runserver import Command
+PORT = 8000
 runserver = Command()
-runserver.execute()
+runserver.execute(addrport='0.0.0.0:%i' % PORT)

d3fa9b2b544abeb417f7e503eb68c659829cd719
Baptiste Lagarde <baptiste.lagarde@centraliens.net>
2013-12-14 18:30:10 +1100

    DRAFT: Redirect runserver() output to display it inside the app

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from kivy.app import App
-from kivy.uix.button import Button
 from android import AndroidService
+from tempfile import NamedTemporaryFile
+from time import sleep
+from threading import Thread
 
 
 class DjandroApp(App):
     def build(self):
-        self.btn = Button(text='Start Django')
-        self.btn.bind(on_press=self.toggle)
         self.service = AndroidService('Django', 'Django is running')
         self.running = False
-        return self.btn
+        self.tmp = NamedTemporaryFile(mode='w+')
+        self.console = Thread(target=self.logging)
 
-    def toggle(self, value):
-        action = self.stop_service if self.running else self.start_service
+    def toggle(self):
+        action = self.stop if self.running else self.start
         action()
         self.running = not self.running
         state = 'ON' if self.running else 'OFF'
-        self.btn.text = "Django is " + state
+        self.root.ids['btn'].text = "Django is " + state
 
-    def start_service(self):
-        self.service.start('')
+    def start(self):
+        self.service.start(self.tmp.name)
+        self.console.start()
 
-    def stop_service(self):
+    def stop(self):
         self.service.stop()
+        self.console.stop()
+
+    def logging(self):
+        label = self.root.ids['console']
+        while True:
+            self.tmp.seek(self.tmp.tell())
+            text = self.tmp.read()
+            if text != '':
+                label.text += text
+            sleep(0.1)
+
 
 if __name__ == '__main__':
     DjandroApp().run()
--- service/main.py
+++ service/main.py
@@ -1,6 +1,11 @@
 import os
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djandro.settings")
 from django.core.management.commands.runserver import Command
 PORT = 8000
+
+logpath = os.getenv('PYTHON_SERVICE_ARGUMENT')
+log = open(logpath, 'w')
 runserver = Command()
+runserver.stdout = log
 runserver.execute(addrport='0.0.0.0:%i' % PORT)
